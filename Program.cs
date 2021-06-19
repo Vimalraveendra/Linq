a584dd4039818cd10dc2967cbe4700384ffdea92
Vimal Raveendran <vimalraveendra@gmail.com>
2021-06-19 21:57:59 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Linq
+{
+    class Program
+    {
+
+        static string getName()
+        {
+            return "John";
+        }
+        static void printName()
+        {
+            var name = getName();
+            Console.WriteLine(name);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            printName();
+        }
+    }
+}
